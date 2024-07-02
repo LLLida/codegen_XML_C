628136ef2950ef42be62cc9a3cdc8e361e6a2e85
LLLida <0adilmohammad0@gmail.com>
2024-07-03 00:07:17 +0300

    Parsing nodes
    I'm kind of dissapointed that I couldn't end the project today.
    
    Next:
     - write functions for parsing nodes
     - write functions for backend
     - write backend implementation (easy)

--- nwogen/codegen.cpp
+++ nwogen/codegen.cpp
@@ -5,9 +5,17 @@
 #include <string>
 #include <unordered_map>
 
+using namespace std::literals;
+
 namespace nwogen {
 
-static void parseObject(rapidxml::xml_node<>* node);
+static std::unordered_map<int, std::vector<int>> parseEdges(rapidxml::xml_node<>* rootNode);
+
+static std::shared_ptr<Block> parseInport(rapidxml::xml_node<>* blockNode);
+static std::shared_ptr<Block> parseOutport(rapidxml::xml_node<>* blockNode);
+static std::shared_ptr<Block> parseSum(rapidxml::xml_node<>* blockNode);
+static std::shared_ptr<Block> parseGain(rapidxml::xml_node<>* blockNode);
+static std::shared_ptr<Block> parseUnitDelay(rapidxml::xml_node<>* blockNode);
 
 CodeGenerator::CodeGenerator(rapidxml::xml_document<>& document)
 {
@@ -16,72 +24,42 @@ CodeGenerator::CodeGenerator(rapidxml::xml_document<>& document)
     throw ParseError("Expected root node to be called \"System\"");
   }
 
-  std::unordered_map<int, rapidxml::xml_node<>*> sid_to_node;
-  std::unordered_map<int, std::vector<int>> edges;
+  using ParseFunction = std::shared_ptr<Block>(*)(rapidxml::xml_node<>*);
+  std::unordered_map<std::string, ParseFunction> parseFunctions;
+  parseFunctions["Inport"] = parseInport;
+  parseFunctions["Outport"] = parseOutport;
+  parseFunctions["Sum"] = parseSum;
+  parseFunctions["Gain"] = parseGain;
+  parseFunctions["UnitDelay"] = parseUnitDelay;
 
+  std::unordered_map<int, std::vector<int>> edges = parseEdges(systemNode);
+  std::vector<int> sid_order;
+  std::unordered_map<int, std::shared_ptr<Block>> blocks;
 
-
-  for (rapidxml::xml_node<>* node = systemNode->first_node(); node; node = node->next_sibling()) {
-
-    if (strcmp(node->name(), "Block") == 0) {
-      auto blockType = node->first_attribute("BlockType");
-      if (blockType == nullptr) {
-	throw ParseError("Expected block node to have attribute \"BlockType\"");
-      }
-      auto name = node->first_attribute("Name");
-      if (name == nullptr) {
-	throw ParseError("Expected block node to have attribute \"Name\"");
-      }
-      auto sid = node->first_attribute("SID");
-      if (sid == nullptr) {
-	throw ParseError("Expected block node to have attribute \"sid\"");
-      }
-      int sidValue = atoi(sid->value());
-
-      if (sid_to_node.find(sidValue) != sid_to_node.end()) {
-	throw ParseError("Found two blocks with the same SID");
-      }
-
-      sid_to_node[sidValue] = node;
-
-    } else if (strcmp(node->name(), "Line") == 0) {
-
-      int src = -1, dst = -1;
-      for (auto child = node->first_node("P"); child; child = child->next_sibling("P")) {
-
-	auto name = child->first_attribute("Name");
-	if (name) {
-	  if (strcmp(name->value(), "Src") == 0) {
-	    std::cout << "\t" << child->value() << " " << atoi(child->value()) << "\n";
-	    src = atoi(child->value());
-	  } else if (strcmp(name->value(), "Dst") == 0) {
-	    std::cout << "\t" << child->value() << " " << atoi(child->value()) << "\n";
-	    dst = atoi(child->value());
-	  }
-	}
-
-      }
-      if (dst != -1) {
-	edges[dst].push_back(src);
-      }
-
-      for (auto branch = node->first_node("Branch"); branch; branch = branch->next_sibling("Branch")) {
-
-	for (auto child = branch->first_node("P"); child; child = child->next_sibling("P")) {
-	  auto name = child->first_attribute("Name");
-	  if (name) {
-	    if (strcmp(name->value(), "Dst") == 0) {
-	      dst = atoi(child->value());
-	    }
-	  }
-	}
-	edges[dst].push_back(src);
-
-      }
-
-    } else {
-      throw ParseError("Expected node to be called \"Block\" or \"Line\"");
+  for (rapidxml::xml_node<>* node = systemNode->first_node("Block"); node; node = node->next_sibling("Block")) {
+    auto blockType = node->first_attribute("BlockType");
+    if (blockType == nullptr) {
+      throw ParseError("Expected block node to have attribute \"BlockType\"");
     }
+    auto name = node->first_attribute("Name");
+    if (name == nullptr) {
+      throw ParseError("Expected block node to have attribute \"Name\"");
+    }
+    auto sid = node->first_attribute("SID");
+    if (sid == nullptr) {
+      throw ParseError("Expected block node to have attribute \"sid\"");
+    }
+    int sidValue = atoi(sid->value());
+
+    if (blocks.find(sidValue) != blocks.end()) {
+      throw ParseError("Found two blocks with the same SID");
+    }
+
+    if (parseFunctions.find(blockType->value()) == parseFunctions.end()) {
+      throw ParseError("Undefined block type: "s + blockType->value());
+    }
+
+    blocks[sidValue] = parseFunctions[blockType->value()](node);
 
   }
 
@@ -99,25 +77,71 @@ void CodeGenerator::generateCode(const Backend& backend, std::ostream& out) {
   // NOTE: generateCode assumes that blocks are already sorted in topological order
 }
 
-
-void parseObject(rapidxml::xml_node<>* node)
+std::unordered_map<int, std::vector<int>> parseEdges(rapidxml::xml_node<>* rootNode)
 {
-  for (auto child = node->first_node(); child; child = child->next_sibling()) {
-    if (strcmp(child->name(), "P") == 0) {
-      auto attribute = child->first_attribute("Name");
-      if (attribute == nullptr) {
-	throw ParseError("Expected child node of block node to have attribute \"Name\"");
+  std::unordered_map<int, std::vector<int>> edges;
+
+  for (rapidxml::xml_node<>* node = rootNode->first_node("Line"); node; node = node->next_sibling("Line")) {
+    int src = -1, dst = -1;
+    for (auto child = node->first_node("P"); child; child = child->next_sibling("P")) {
+
+      auto name = child->first_attribute("Name");
+      if (name) {
+	if (strcmp(name->value(), "Src") == 0) {
+	  std::cout << "\t" << child->value() << " " << atoi(child->value()) << "\n";
+	  src = atoi(child->value());
+	} else if (strcmp(name->value(), "Dst") == 0) {
+	  std::cout << "\t" << child->value() << " " << atoi(child->value()) << "\n";
+	  dst = atoi(child->value());
+	}
       }
-      std::cout << "  Field: " << attribute->value() << "=\"" << child->value() << "\"\n";
-    } else if (strcmp(child->name(), "Port") == 0) {
-      parseObject(child);
-    } else if (strcmp(child->name(), "Branch") == 0) {
-      parseObject(child);
-    } else {
-      std::cout << child->name() << "--\n";
-      throw ParseError("Expected child node of block node to have name \"P\" or \"Port\"");
+
+    }
+    if (dst != -1) {
+      edges[dst].push_back(src);
+    }
+
+    for (auto branch = node->first_node("Branch"); branch; branch = branch->next_sibling("Branch")) {
+
+      for (auto child = branch->first_node("P"); child; child = child->next_sibling("P")) {
+	auto name = child->first_attribute("Name");
+	if (name) {
+	  if (strcmp(name->value(), "Dst") == 0) {
+	    dst = atoi(child->value());
+	  }
+	}
+      }
+      edges[dst].push_back(src);
+
     }
   }
+
+  return edges;
+}
+
+std::shared_ptr<Block> parseInport(rapidxml::xml_node<>* blockNode)
+{
+  return nullptr;
+}
+
+std::shared_ptr<Block> parseOutport(rapidxml::xml_node<>* blockNode)
+{
+  return nullptr;
+}
+
+std::shared_ptr<Block> parseSum(rapidxml::xml_node<>* blockNode)
+{
+  return nullptr;
+}
+
+std::shared_ptr<Block> parseGain(rapidxml::xml_node<>* blockNode)
+{
+  return nullptr;
+}
+
+std::shared_ptr<Block> parseUnitDelay(rapidxml::xml_node<>* blockNode)
+{
+  return nullptr;
 }
 
 }
